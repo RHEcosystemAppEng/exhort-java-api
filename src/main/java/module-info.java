6db068cde4e6d671cadbdc63934a3a1890c1024a
Tomer Figenblat <tfigenbl@redhat.com>
2023-05-30 12:59:45 +0300

    feat: implemented crdaignore comments for maven
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,8 @@ module com.redhat.crda {
   requires com.fasterxml.jackson.annotation;
   requires com.fasterxml.jackson.core;
   requires com.fasterxml.jackson.databind;
-  requires com.fasterxml.jackson.dataformat.xml;
   requires jakarta.annotation;
+  requires java.xml;
 
   opens com.redhat.crda.backend to com.fasterxml.jackson.databind;
   opens com.redhat.crda.providers to com.fasterxml.jackson.databind;

37d86e0f5fc8b7cdc55f3f5431357786ad75b6ad
Tomer Figenblat <tfigenbl@redhat.com>
2023-06-29 14:39:27 +0300

    feat: allow customizing via java properties (#30)
    * chore: look for customizebles in java properties as well as environment variables
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    * docs: update doca for customizing via properties
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>
    
    ---------
    
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>

--- src/test/java/com/redhat/crda/impl/Crda_Api_Test.java
+++ src/test/java/com/redhat/crda/impl/Crda_Api_Test.java
@@ -27,8 +27,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redhat.crda.Provider;
 import com.redhat.crda.backend.AnalysisReport;
 import com.redhat.crda.tools.Ecosystem;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junitpioneer.jupiter.ClearEnvironmentVariable;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
 import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -42,6 +45,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @ExtendWith(MockitoExtension.class)
+@ClearEnvironmentVariable(key="CRDA_SNYK_TOKEN")
 class Crda_Api_Test {
   @Mock
   Provider mockProvider;
@@ -50,7 +54,13 @@ class Crda_Api_Test {
   @InjectMocks
   CrdaApi crdaApiSut;
 
+  @AfterEach
+  void cleanup() {
+    System.clearProperty("CRDA_SNYK_TOKEN");
+  }
+
   @Test
+  @SetEnvironmentVariable(key="CRDA_SNYK_TOKEN", value="snyk-token-from-env-var")
   void stackAnalysisHtml_with_pom_xml_should_return_html_report_from_the_backend()
       throws IOException, ExecutionException, InterruptedException {
     // create a temporary pom.xml file
@@ -67,6 +77,8 @@ class Crda_Api_Test {
     ArgumentMatcher<HttpRequest> matchesRequest = r ->
       r.headers().firstValue("Content-Type").get().equals("fake-content-type") &&
       r.headers().firstValue("Accept").get().equals("text/html") &&
+      // snyk token is set using the environment variable (annotation)
+      r.headers().firstValue("crda-snyk-token").get().equals("snyk-token-from-env-var") &&
       r.method().equals("POST");
 
     // mock and http response object and stub it to return a fake body
@@ -92,6 +104,7 @@ class Crda_Api_Test {
   }
 
   @Test
+  @SetEnvironmentVariable(key="CRDA_SNYK_TOKEN", value="snyk-token-from-env-var")
   void stackAnalysis_with_pom_xml_should_return_json_object_from_the_backend()
     throws IOException, ExecutionException, InterruptedException {
     // create a temporary pom.xml file
@@ -104,10 +117,15 @@ class Crda_Api_Test {
     given(mockProvider.provideStack(tmpFile))
       .willReturn(new Provider.Content("fake-body-content".getBytes(), "fake-content-type"));
 
+    // we expect this to be ignored because tokens from env vars takes precedence
+    System.setProperty("CRDA_SNYK_TOKEN", "snyk-token-from-property");
+
     // create an argument matcher to make sure we mock the response for the right request
     ArgumentMatcher<HttpRequest> matchesRequest = r ->
       r.headers().firstValue("Content-Type").get().equals("fake-content-type") &&
         r.headers().firstValue("Accept").get().equals("application/json") &&
+        // snyk token is set using the environment variable (annotation) - ignored the one set in properties
+        r.headers().firstValue("crda-snyk-token").get().equals("snyk-token-from-env-var") &&
         r.method().equals("POST");
 
     // load dummy json and set as the expected analysis
@@ -152,10 +170,15 @@ class Crda_Api_Test {
     given(mockProvider.provideComponent(targetPom))
       .willReturn(new Provider.Content("fake-body-content".getBytes(), "fake-content-type"));
 
+    // we expect this to picked up because no env var to take precedence
+    System.setProperty("CRDA_SNYK_TOKEN", "snyk-token-from-property");
+
     // create an argument matcher to make sure we mock the response for the right request
     ArgumentMatcher<HttpRequest> matchesRequest = r ->
       r.headers().firstValue("Content-Type").get().equals("fake-content-type") &&
         r.headers().firstValue("Accept").get().equals("application/json") &&
+        // snyk token is set using properties which is picked up because no env var specified
+        r.headers().firstValue("crda-snyk-token").get().equals("snyk-token-from-property") &&
         r.method().equals("POST");
 
     // load dummy json and set as the expected analysis

a7f1eb085e92a42cc455d19a3e054c48f26c9040
Zvi Grinberg <zgrinber@redhat.com>
2023-11-01 13:31:32 +0200

    test: adjust expected component sbom to be aligned with mocked npm ls versions
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- src/test/java/com/redhat/exhort/providers/Javascript_Npm_Provider_Test.java
+++ src/test/java/com/redhat/exhort/providers/Javascript_Npm_Provider_Test.java
@@ -25,11 +25,6 @@ import java.nio.file.Path;
 import java.util.stream.Stream;
 
 import com.redhat.exhort.tools.Operations;
-import net.bytebuddy.asm.Advice;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
